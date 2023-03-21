fb7cd5e6285ed803e981f59380835c9ee72ddcaf
EduardoMollinedo <eduu.mch@gmail.com>
2023-03-21 11:38:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { db } from './db.js'
+
+import mysql from "mysql";
+import cors from "cors";
+import { PORT } from "./config.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.json("hello worldsdasd");
+});
+
+app.get("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "SELECT ncodcol,nestcol,ndnicol  FROM colegiados where ncodcol = ?";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+app.get("/pagos/:id", (req, res) => {
+	const bookId = req.params.id;
+	const q = `SELECT vserdoc, vnumdoc, nsubtot, ncodcli FROM movimientoscab WHERE vnumdoc LIKE '%${bookId}'`;
+  
+	db.query(q, [bookId], (err, data) => {
+	  if (err) {
+		console.log(err);
+		return ("nro de serie no encontrado");
+	  }
+	  return res.json(data);
+	});
+  });
+
+
+app.listen(PORT, () => {
+  console.log("Connected to backend.");
+});
