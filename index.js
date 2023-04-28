e635a10e2a8030e3da597fc8ba018a2aadd954b3
EduardoMollinedo <41025653+EduardoMollinedo@users.noreply.github.com>
2023-04-28 12:50:50 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -20,13 +20,10 @@ app.get("/colegiados/:id", (req, res) => {
     if (err) {
       console.log(err);
       return res.json(err);
-	  db.end();
-
     }
     return res.json(data);
-	db.end();
-
   });
+  db.end();
 });
 
 app.get("/pagos/:id/:id2/:id3", (req, res) => {
@@ -39,28 +36,26 @@ app.get("/pagos/:id/:id2/:id3", (req, res) => {
     if (err) {
       console.log(err);
       return "nro de serie no encontrado";
-	  db.end();
     }
     return res.json(data);
-	db.end();
   });
+  db.end();
 });
 
 app.get("/listaPagos/:id/:id2", (req, res) => {
-	const correlativo = req.params.id;
-	const nroSerie = req.params.id2;
-	db.connect();
-	const q = `SELECT  vserdoc, vnumdoc,  ncodcli , vtipope, nvaluni , vdesope  FROM movimientodetalle WHERE vnumdoc LIKE '%${correlativo}' and vserdoc LIKE '%${nroSerie} `;
-	db.query(q, [correlativo, nroSerie], (err, data) => {
-	  if (err) {
-		console.log(err);
-		return "nro de serie no encontrado";
-		db.end();
-	  }
-	  return res.json(data);
-	  db.end();
-	});
+  const correlativo = req.params.id;
+  const nroSerie = req.params.id2;
+  db.connect();
+  const q = `SELECT  vserdoc, vnumdoc,  ncodcli , vtipope, nvaluni , vdesope  FROM movimientodetalle WHERE vnumdoc LIKE '%${correlativo}' and vserdoc LIKE '%${nroSerie}'	 `;
+  db.query(q, [correlativo, nroSerie], (err, data) => {
+    if (err) {
+      console.log(err);
+      return "nro de serie no encontrado";
+    }
+    return res.json(data);
   });
+  db.end();
+});
 
 app.listen(PORT, () => {
   console.log("Conectado al backend ");
