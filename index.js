27d5cb69fcf380608bf9a4651ffa037dcdcac0db
EduardoMollinedo <41025653+EduardoMollinedo@users.noreply.github.com>
2023-04-28 12:56:21 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get("/colegiados/:id", (req, res) => {
   const bookId = req.params.id;
   db.connect();
   const q = "SELECT ncodcol,nestcol,ndnicol  FROM colegiados where ncodcol = ?";
+
   db.query(q, [bookId], (err, data) => {
     if (err) {
       console.log(err);
@@ -31,7 +32,9 @@ app.get("/pagos/:id/:id2/:id3", (req, res) => {
   const nroSerie = req.params.id2;
   const monto = req.params.id3;
   db.connect();
+
   const q = `SELECT vserdoc, vnumdoc, nvaltot, ncodcli FROM movimientoscab WHERE vnumdoc LIKE '%${correlativo}' and vserdoc LIKE '%${nroSerie}' and nvaltot = ${monto} `;
+
   db.query(q, [correlativo, nroSerie, monto], (err, data) => {
     if (err) {
       console.log(err);
@@ -42,21 +45,6 @@ app.get("/pagos/:id/:id2/:id3", (req, res) => {
   db.end();
 });
 
-app.get("/listaPagos/:id/:id2", (req, res) => {
-  const correlativo = req.params.id;
-  const nroSerie = req.params.id2;
-  db.connect();
-  const q = `SELECT  vserdoc, vnumdoc,  ncodcli , vtipope, nvaluni , vdesope  FROM movimientodetalle WHERE vnumdoc LIKE '%${correlativo}' and vserdoc LIKE '%${nroSerie}'	 `;
-  db.query(q, [correlativo, nroSerie], (err, data) => {
-    if (err) {
-      console.log(err);
-      return "nro de serie no encontrado";
-    }
-    return res.json(data);
-  });
-  db.end();
-});
-
 app.listen(PORT, () => {
-  console.log("Conectado al backend ");
+  console.log("Connected to backend.");
 });
