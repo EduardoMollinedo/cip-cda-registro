9586a942bb18bb071de52a4355b5398b8b36f8e3
EduardoMollinedo <41025653+EduardoMollinedo@users.noreply.github.com>
2023-05-06 23:53:53 -0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-export const PORT = process.env.PORT || 8800
-
-export const DB_HOST = process.env.DB_HOST || 'ciparequipa.org.pe'
-export const DB_USER = process.env.DB_USER || 'superadmin'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Pat1Pat1'
-export const DB_NAME = process.env.DB_NAME || 'bakuprecuperado'
-export const DB_PORT = process.env.DB_PORT || 3306
